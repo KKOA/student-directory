b6af4b28b11d3a08664acb1c5e2af038247deb8e
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-14 23:56:25 +0100

    Refactored code to use an array for storing student's names and outputting total number of students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
-student_count = 11
-#Outputted the list students
+#Added array to hold students
+students =
+[
+  "Dr. Hannibal Lector",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
+#Outputted the list students from array
 puts "The students of Villians Academy"
 puts "-------------"
-puts "Dr. Hannibal Lector"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+
 # Unlike puts, print does not add new character so have use escape character (\n)
-puts "Overall, we have #{student_count} great students"
+# Using array.count method to count the number of students rather than hard coding the value
+puts "Overall, we have #{students.count} great students"
