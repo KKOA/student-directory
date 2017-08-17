8365f3176a58727416a50edbc94988b6dd09f5c2
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-17 17:13:33 +0100

    Modify print method to only output names that are less than 12 characters and start with  letter a

--- directory.rb
+++ directory.rb
@@ -21,7 +21,7 @@ def print(names) #This method overwrite existing print method
   current_pos = 0
   namesLength = names.length
   while current_pos < namesLength #loop while current pos less length of names array
-    if(names[current_pos][:name].length < 12) #output if :name is less than 12 characters
+    if(names[current_pos][:name].length < 12 && names[current_pos][:name][0].downcase == "a") #output if :name is less than 12 characters and begins with specific character
       puts "#{current_pos+1}. #{names[current_pos][:name]} (#{names[current_pos][:cohort]} cohort)"
     end
     current_pos += 1
