98edc4cd0d2b6e6240524b30ed6e4317f0067417
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-17 20:48:51 +0100

    Replaced "chomp" with "strip" to prevent names contain only spaces, from being accepted. Also, cleaned up grammar & presentation.

--- directory.rb
+++ directory.rb
@@ -1,21 +1,22 @@
 #Define methods
 def input_students #method allows the user dynamically input students
   students = [] #empty array
-  puts "Please enter the names of the students. Hit enter twice to exit"
-  name = gets.chomp #Get user input
+  puts "Please enter a student name. Hit enter twice to exit"
+  name = gets.strip #Get user input and remove leading and trailing whitespaces
+  name = name.split(" ").map{|x| x.capitalize}.join(" ") # capitalize first letter of each word e.g. aaron smith => Aaron Smith
   until name.empty? do #repeat code while name is not empty
     puts "Please enter your hobbies"
-    hobbies =gets.chomp
+    hobbies =gets.strip
     puts "Please enter Country of Birth"
-    country_of_birth = gets.chomp
+    country_of_birth = gets.strip
     puts "Please enter your height in cm"
-    height =gets.chomp.to_i
+    height =gets.strip.to_i
     students << {:name => name, :cohort => :november, :hobbies => hobbies ,:country_of_birth => country_of_birth, :height => height} #add ended of array
     msg = "Now we have #{students.count} student"
     msg << "s" if students.count > 1
     puts msg,""
-    puts "Please enter another names of the students. Hit enter twice to exit"
-    name = gets.chomp #ask user for another, also need to prevent infinite loop
+    puts "Please enter another student name. Hit enter twice to exit"
+    name = gets.strip #ask user for another, also need to prevent infinite loop
   end
   return students # return array
 end
@@ -34,9 +35,9 @@ def print(names) #This method overwrite existing print method
       puts("#{current_pos+1}. "+
            "#{names[current_pos][:name]} "+
            "(#{names[current_pos][:cohort]} cohort) "+
-           "hobbies: #{names[current_pos][:hobbies]} "+
-           "country of birth: #{names[current_pos][:country_of_birth]} "+
-           "height: #{names[current_pos][:height]} cm "
+           "Hobbies: #{names[current_pos][:hobbies]} "+
+           "Country of birth: #{names[current_pos][:country_of_birth]} "+
+           "Height: #{names[current_pos][:height]} cm "
            )
     end
     current_pos += 1
