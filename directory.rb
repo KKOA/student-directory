d139b89bfcd66dee59f39c5515daac31ccae23ab
keith(Desktop - mac) <keithamoah@yahoo.co.uk>
2017-08-20 11:12:01 +0100

    Prepended STDOUT to all puts outputting to the screen. Refactored code to use newly added question_answer method

--- directory.rb
+++ directory.rb
@@ -1,41 +1,42 @@
 @students = [] #Define students array so menu option 2 can be called before menu option 1
 
 #Define methods
+def question_answer(question) #Takes question as input and return users input
+  STDOUT.puts(question)
+  return STDIN.gets.strip #Get user input and remove leading and trailing whitespaces
+  #gets method reads from the list of files supplied as arguments, only defaulting to the keyboard (or, standard input stream, to be precise) if there are no files.
+  #Because passing argument to file on startup, we need prepend gets with reference of STDIN for all place where we expecting user input from keyboard
+end
+
 def input_students #method allows the user dynamically input students
   all_cohorts =['january','february','march','april','may','june','july','august','september','october','november','december']
-  puts("Please enter a student name. Hit enter twice to exit")
-  name = STDIN.gets.strip #Get user input and remove leading and trailing whitespaces
-  name = name.split(" ").map{|x| x.capitalize}.join(" ") # capitalize first letter of each word e.g. aaron smith => Aaron Smith
+  name = question_answer("Please enter a student name. Hit enter twice to exit")
   until name.empty? do #repeat code while name is not empty
-    puts("Please enter the cohort month name (i.e august)")
-    puts("that the student belong to or leave blank for current month")
-    cohort = STDIN.gets.strip.downcase
+    name = name.split(" ").map{|x| x.capitalize}.join(" ") # capitalize first letter of each word e.g. aaron smith => Aaron Smith
+    cohort = question_answer("Please enter the cohort month name (i.e august)\n that the student belong to or leave blank for current month").downcase
     until all_cohorts.include?(cohort)
       if(cohort.length == 0)
         cohort = Time.now.strftime("%B").downcase # get the current month
         break
       end
-      puts("#{cohort} is an invalid cohort month name")
-      puts("Please enter the cohort month name that the student belong to")
-      cohort = STDIN.gets.strip.downcase
+      STDOUT.puts("#{cohort} is an invalid cohort month name")
+      cohort = question_answer("Please enter the cohort month name (i.e august) \nthat the student belong to or leave blank for current month").downcase
     end
-    puts("Please enter your hobbies")
-    hobbies = STDIN.gets.strip
-    puts("Please enter country of birth")
-    country_of_birth = STDIN.gets.strip
-    puts("Please enter your height in cm")
-    height = STDIN.gets.strip.to_i
-    @students << {:name => name, :cohort => cohort.to_sym, :hobbies => hobbies ,:country_of_birth => country_of_birth, :height => height} #add ended of array
+    hobbies = question_answer("Please enter your hobbies")
+    country_of_birth = question_answer("Please enter country of birth")
+    height = question_answer("Please enter your height in cm")
+    @students << {:name => name, :cohort => cohort.to_sym, :hobbies => hobbies ,:country_of_birth => country_of_birth, :height => height}
+    #add ended of array
     msg = "Now we have #{@students.count} student"
     msg << "s" if(@students.count > 1)
-    puts(msg,"")
-    puts("Please enter another student name. Hit enter twice to exit")
-    name = STDIN.gets.strip #ask user for another, also need to prevent infinite loop
+    STDOUT.puts(msg,"")
+    name = question_answer("Please enter another student name. Hit enter twice to exit")
+    #ask user for another, also need to prevent infinite loop
   end
 end
 
 def print_header #output the header
-  puts("", "The students of Villians Academy".center(70),("-"*68).center(70))
+  STDOUT.puts("", "The students of Villians Academy".center(70),("-"*68).center(70))
 end
 
 def print_student_lists
@@ -44,7 +45,7 @@ def print_student_lists
   return nil if(studentLength == 0) #Prevent looping through students array if there are no students
   while(current_pos < studentLength) #loop while current pos less length of student array
     if((@students[current_pos][:name].length) < 12 && (@students[current_pos][:name][0].downcase == "a")) #output if :name is less than 12 characters and begins with specific character
-      puts("#{current_pos+1}. "+
+      STDOUT.puts("#{current_pos+1}. "+
            "#{@students[current_pos][:name]} "+
            "(#{@students[current_pos][:cohort]} cohort) "+
            "Hobbies: #{@students[current_pos][:hobbies]} "+
@@ -57,15 +58,15 @@ def print_student_lists
 end
 
 def print_footer # Takes array of students and return number students using count method in a message
-  puts("\nOverall, we have #{@students.count} great students\n\n")
+  STDOUT.puts("\nOverall, we have #{@students.count} great students\n\n")
 end
 
 def print_menu
-  puts("","Menu".center(70),("-"*68).center(70))
+  STDOUT.puts("","Menu".center(70),("-"*68).center(70))
   menu =['Input the students','Show students','Save list to file','Load list from file','','','','','Exit']
   menu.each_with_index do | menu_item, key |
     next if menu_item == '' # Do not display empty menu items
-    puts("#{key+1}. #{menu_item}")
+    STDOUT.puts("#{key+1}. #{menu_item}")
   end
 end
 
@@ -88,7 +89,7 @@ def save_students #save data to file
   end
   file.close #close file. Only writes to file once closed it
   STDOUT.puts "Closing File" # STDOUT.puts an puts are same
-  puts "Data saved to #{filename}"
+  STDOUT.puts "Data saved to #{filename}"
 end
 
 def load_students(filename="student.csv")
@@ -98,19 +99,19 @@ def load_students(filename="student.csv")
     @students << {:name => name, :cohort => cohort.to_sym, :hobbies => hobbies ,:country_of_birth => country_of_birth, :height => height}
   end
   file.close #close file
-  puts("Closing File")
-  puts("Loaded #{@students.count} from #{filename}")
+  STDOUT.puts("Closing File")
+  STDOUT.puts("Loaded #{@students.count} from #{filename}")
 end
 
 def process(selection)
   case selection
   when "1"
-    puts("")
+    STDOUT.puts("")
     input_students
     msg = "#{@students.count} student"
     msg << "s" if @students.count > 1
     msg << " added"
-    puts(msg)
+    STDOUT.puts(msg)
   when "2"
     show_students
   when "3"
@@ -118,25 +119,18 @@ def process(selection)
   when "4"
     load_students
   when "9"
+    STDOUT.puts("","Exiting Program","")
     exit
   else
-    puts("'#{selection}' is an invaid option. Please try again")
+    STDOUT.puts("'#{selection}' is an invaid option. Please try again")
   end
 end
 
 def interactive_menu
   loop do
-    print_menu #1 print the menu
-
-    #2 read the input and save it into variable
-    puts("","Please select an option ")
-    selection =   STDIN.gets.strip
-    #gets method reads from the list of files supplied as arguments, only defaulting to the keyboard (or, standard input stream, to be precise) if there are no files.
-    #Because passing argument to file on startup, we need prepend gets with reference of STDIN for all place where we expecting user input from keyboard
-
-    process(selection) #3 perform action
+    print_menu #1. print the menu
+    process(question_answer("Please select an option ")) #2. read the input and #3. perform action
   end
-  puts("","Exiting Program","")
 end
 
 def try_load
@@ -147,7 +141,7 @@ def try_load
   if(File.exists?(filename))#check file exist
     load_students(filename)
   else #file does not exist
-    puts("Sorry #{filename} does not exist.")
+    STDOUT.puts("Sorry #{filename} does not exist.")
   end
 end
 
@@ -159,5 +153,4 @@ Future imporvements
 Edit student's detail
 Delete student's detail
 Enable user control the filter on number records they can see
-Add question method which takes question as an argument and return the answer
 =end
